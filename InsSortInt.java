ad01503d87880d9e9a76d7a5504e3a21b85b6ac3
Cooper Shine <166638981+shinecoop@users.noreply.github.com>
2024-08-28 19:26:13 +0000

    inserting and sorting and stuff

--- InsSortInt.java
+++ InsSortInt.java
@@ -1,3 +1,45 @@
+import java.util.Scanner;
+import java.io.*;
+import java.util.ArrayList;
+
 public class InsSortInt {
+    public static void main(String args[]) throws FileNotFoundException{
+        
+        File file = new File("./randInts.txt");
+        Scanner s = new Scanner(file);
+
+        ArrayList<Integer> in = new ArrayList<>();
+
+        while(s.hasNextInt()){
+        
+            in.add(s.nextInt());
+
+        }
+        
+
+
+        Integer[] arr = new Integer[in.size()];
+        in.toArray(arr);
+//insertion sort
+        for(int i=1; i<arr.length; i++){
+            int count = i-1;
+            int key=arr[i];
+
+            while(count>=0 && arr[count]>=key){
+
+                arr[count+1] = arr[count];
+                count--;
+
+            }
+
+            arr[count+1]=key;        
+
+
+        }
+
+        System.out.println("Smallest Value: "+ arr[0]+" and Largest Value: "+arr[arr.length-1]);
+    }
+
+
 
 }
